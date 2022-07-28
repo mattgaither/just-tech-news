f2ba245b07e9034802171720eab0b361bddbc367
mattgaither <benjamin.gaither@shipt.com>
2022-07-28 08:47:45 -0500

    finished the module

--- config/connection.js
+++ config/connection.js
@@ -13,5 +13,13 @@ if (process.env.JAWSDB_URL) {
     port: 3306
   });
 }
+// create connection to our db
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     });
 
 module.exports = sequelize;
